7b91aef4139577815a73466df8ee2d2a111763f0
tomy08 <tomasesantacruz@gmail.com>
2024-05-12 19:47:59 -0300

    add function actualizarRanking and minnor changes in tables

---
+++ funciones.sql
@@ -0,0 +1,189 @@
+USE table_tennis;
+
+-- Creacion de funciones
+DROP FUNCTION IF EXISTS obtenerGanadorPartido;
+
+DELIMITER / / CREATE FUNCTION obtenerGanadorPartido (ID_partido INT) RETURNS INT READS SQL DATA BEGIN DECLARE ganador INT;
+
+DECLARE contador_jugador1 INT DEFAULT 0;
+
+DECLARE contador_jugador2 INT DEFAULT 0;
+
+-- Contar los sets ganados por cada jugador en el partido dado
+SELECT
+    COUNT(*) INTO contador_jugador1
+FROM
+    sets
+WHERE
+    ID_partido = obtenerGanadorPartido.ID_partido
+    AND ID_ganador = sets.ID_jugador1;
+
+SELECT
+    COUNT(*) INTO contador_jugador2
+FROM
+    sets
+WHERE
+    ID_partido = obtenerGanadorPartido.ID_partido
+    AND ID_ganador = sets.ID_jugador2;
+
+IF contador_jugador1 > contador_jugador2 THEN
+SET
+    ganador = sets.ID_jugador1;
+
+ELSEIF contador_jugador2 > contador_jugador1 THEN
+SET
+    ganador = sets.ID_jugador2;
+
+ELSE
+SET
+    ganador = NULL;
+
+END IF;
+
+RETURN ganador;
+
+END / / DELIMITER;
+
+DROP FUNCTION IF EXISTS actualizarRanking;
+
+DELIMITER / / CREATE FUNCTION actualizarRanking (ID_partido INT) RETURNS VOID BEGIN DECLARE instancia_actual VARCHAR(50);
+
+DECLARE ranking_actual_jugador1 INT;
+
+DECLARE ranking_actual_jugador2 INT;
+
+DECLARE ganador_partido INT;
+
+SELECT
+    instancia INTO instancia_actual
+FROM
+    partido
+WHERE
+    ID = ID_partido;
+
+SELECT
+    rating INTO ranking_actual_jugador1
+FROM
+    jugador
+WHERE
+    ID = (
+        SELECT
+            ID_jugador1
+        FROM
+            partido
+        WHERE
+            ID = ID_partido
+    );
+
+SELECT
+    rating INTO ranking_actual_jugador2
+FROM
+    jugador
+WHERE
+    ID = (
+        SELECT
+            ID_jugador2
+        FROM
+            partido
+        WHERE
+            ID = ID_partido
+    );
+
+SELECT
+    obtenerGanadorPartido (ID_partido) INTO ganador_partido;
+
+IF instancia_actual = 'final' THEN IF ganador_partido IS NOT NULL THEN
+SET
+    ranking_actual_jugador1 = ranking_actual_jugador1 + 45;
+
+SET
+    ranking_actual_jugador2 = ranking_actual_jugador2 + 45;
+
+END IF;
+
+ELSEIF instancia_actual = 'semifinal' THEN
+SET
+    ranking_actual_jugador1 = ranking_actual_jugador1 + 30;
+
+SET
+    ranking_actual_jugador2 = ranking_actual_jugador2 + 30;
+
+ELSEIF instancia_actual = 'cuartos' THEN
+SET
+    ranking_actual_jugador1 = ranking_actual_jugador1 + 15;
+
+SET
+    ranking_actual_jugador2 = ranking_actual_jugador2 + 15;
+
+ELSEIF instancia_actual = 'octavos' THEN
+SET
+    ranking_actual_jugador1 = ranking_actual_jugador1 + 2;
+
+SET
+    ranking_actual_jugador2 = ranking_actual_jugador2 + 2;
+
+ELSEIF instancia_actual = 'dieciseisavos' THEN
+SET
+    ranking_actual_jugador1 = ranking_actual_jugador1 - 5;
+
+SET
+    ranking_actual_jugador2 = ranking_actual_jugador2 - 5;
+
+ELSEIF instancia_actual = 'treintaidosavos' THEN
+SET
+    ranking_actual_jugador1 = ranking_actual_jugador1 - 10;
+
+SET
+    ranking_actual_jugador2 = ranking_actual_jugador2 - 10;
+
+ELSE RETURN;
+
+END IF;
+
+IF instancia_actual = 'final'
+AND ganador_partido IS NOT NULL THEN IF ganador_partido = (
+    SELECT
+        ID_jugador1
+    FROM
+        partido
+    WHERE
+        ID = ID_partido
+) THEN
+SET
+    ranking_actual_jugador1 = ranking_actual_jugador1 + 25;
+
+ELSE
+SET
+    ranking_actual_jugador2 = ranking_actual_jugador2 + 25;
+
+END IF;
+
+END IF;
+
+UPDATE jugador
+SET
+    rating = ranking_actual_jugador1
+WHERE
+    ID = (
+        SELECT
+            ID_jugador1
+        FROM
+            partido
+        WHERE
+            ID = ID_partido
+    );
+
+UPDATE jugador
+SET
+    rating = ranking_actual_jugador2
+WHERE
+    ID = (
+        SELECT
+            ID_jugador2
+        FROM
+            partido
+        WHERE
+            ID = ID_partido
+    );
+
+END / / DELIMITER;
---
+++ tablas.sql
@@ -0,0 +1,88 @@
+-- Base de datos: table_tennis
+CREATE DATABASE table_tennis;
+
+USE table_tennis;
+
+-- Borrar tablas si existen
+DROP TABLE IF EXISTS sets;
+
+DROP TABLE IF EXISTS partido;
+
+DROP TABLE IF EXISTS arbitro;
+
+DROP TABLE IF EXISTS jugador;
+
+DROP TABLE IF EXISTS club;
+
+DROP TABLE IF EXISTS torneo;
+
+-- Creación de tablas
+CREATE TABLE
+    club (
+        ID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        nombre VARCHAR(50) NOT NULL,
+        ubicacion VARCHAR(50) NOT NULL,
+        fecha_creacion DATE NOT NULL
+    );
+
+CREATE TABLE
+    jugador (
+        ID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        nombre VARCHAR(50) NOT NULL,
+        apellido VARCHAR(50) NOT NULL,
+        fecha_nac DATE NOT NULL,
+        rating INT NOT NULL,
+        ID_club INT,
+        FOREIGN KEY (ID_club) REFERENCES club (ID)
+    );
+
+CREATE TABLE
+    torneo (
+        ID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        nombre_torneo VARCHAR(50) NOT NULL,
+        fecha DATETIME NOT NULL,
+        categoria INT NOT NULL,
+        localidad VARCHAR(50) NOT NULL
+    );
+
+CREATE TABLE
+    arbitro (
+        ID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        nombre VARCHAR(50) NOT NULL,
+        apellido VARCHAR(50) NOT NULL,
+        fecha_nac DATE NOT NULL
+    );
+
+CREATE TABLE
+    partido (
+        ID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        ID_arbitro INT NOT NULL,
+        ID_jugador1 INT NOT NULL,
+        ID_jugador2 INT NOT NULL,
+        ID_torneo INT NOT NULL,
+        instancia VARCHAR(50) NOT NULL,
+        FOREIGN KEY (ID_jugador1) REFERENCES jugador (ID),
+        FOREIGN KEY (ID_jugador2) REFERENCES jugador (ID),
+        FOREIGN KEY (ID_torneo) REFERENCES torneo (ID),
+        FOREIGN KEY (ID_arbitro) REFERENCES arbitro (ID),
+        CONSTRAINT instancia_valores_permitidos CHECK (
+            instancia IN (
+                'final',
+                'semifinal',
+                'cuartos',
+                'octavos',
+                'dieciseisavos',
+                'treintaidosavos'
+            )
+        )
+    );
+
+CREATE TABLE
+    sets (
+        ID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        ID_partido INT,
+        numero_set INT,
+        player1_games_won INT,
+        player2_games_won INT,
+        FOREIGN KEY (ID_partido) REFERENCES partido (ID)
+    );
